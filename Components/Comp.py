237c7e68e843670019ac08684ab9a902c2ee8286
rickc <rickcrist99@gmail.com>
2025-05-29 11:53:06 -0600

    Selection rectangles method

--- Components/Comp.py
+++ Components/Comp.py
@@ -11,18 +11,28 @@ class Comp(QGraphicsItem):
         # self.setAcceptHoverEvents(True)
 
     def boundingRect(self) -> QRectF:
+        """ Sets the size of the item as a bounding rectangle 
+            OVERRIDE THIS METHOD IN DERIVED CLASSES """
         return QRectF(0, 0, 100, 100)
 
     def paint(self, painter: QPainter, option: QStyleOptionGraphicsItem, widget: QWidget = None) -> None:
+        """ Paints the item on the scene
+            If selected, draws a dashed outline
+            OVERRIDE THIS METHOD IN DERIVED CLASSES """
+        # Set the pen and brush for the item
         painter.setBrush(QBrush(Qt.GlobalColor.blue))
         painter.drawRect(self.boundingRect())
 
         # Draw selection dashed outline if selected
         if option.state & QStyle.StateFlag.State_Selected:
-            pen = QPen(Qt.GlobalColor.yellow)
-            pen.setStyle(Qt.PenStyle.DashLine)
-            pen.setCapStyle(Qt.PenCapStyle.FlatCap)
-            pen.setWidth(2)
-            painter.setPen(pen)
-            painter.setBrush(Qt.BrushStyle.NoBrush)
-            painter.drawRect(self.boundingRect())
+            self.draw_selection_outline(painter)
+
+    def draw_selection_outline(self, painter: QPainter) -> None:
+        """ Draws a selection outline around the item """
+        pen = QPen(Qt.GlobalColor.yellow)
+        pen.setStyle(Qt.PenStyle.DashLine)
+        pen.setCapStyle(Qt.PenCapStyle.FlatCap)
+        pen.setWidth(2)
+        painter.setPen(pen)
+        painter.setBrush(Qt.BrushStyle.NoBrush)
+        painter.drawRect(self.boundingRect())
