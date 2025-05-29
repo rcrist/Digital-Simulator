eb74359dbd29694e372453b6577157d21c94362d
rickc <rickcrist99@gmail.com>
2025-05-29 11:41:35 -0600

    Draw selection rectangle

--- Components/Comp.py
+++ Components/Comp.py
@@ -8,7 +8,7 @@ class Comp(QGraphicsItem):
         self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsMovable)
         self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemIsSelectable)
         self.setFlag(QGraphicsItem.GraphicsItemFlag.ItemSendsGeometryChanges)
-        self.setAcceptHoverEvents(True)
+        # self.setAcceptHoverEvents(True)
 
     def boundingRect(self) -> QRectF:
         return QRectF(0, 0, 100, 100)
@@ -16,3 +16,13 @@ class Comp(QGraphicsItem):
     def paint(self, painter: QPainter, option: QStyleOptionGraphicsItem, widget: QWidget = None) -> None:
         painter.setBrush(QBrush(Qt.GlobalColor.blue))
         painter.drawRect(self.boundingRect())
+
+        # Draw selection dashed outline if selected
+        if option.state & QStyle.StateFlag.State_Selected:
+            pen = QPen(Qt.GlobalColor.yellow)
+            pen.setStyle(Qt.PenStyle.DashLine)
+            pen.setCapStyle(Qt.PenCapStyle.FlatCap)
+            pen.setWidth(2)
+            painter.setPen(pen)
+            painter.setBrush(Qt.BrushStyle.NoBrush)
+            painter.drawRect(self.boundingRect())
